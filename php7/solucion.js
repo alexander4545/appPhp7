bcf71dd168e82a5e233a343b4fe6953c2a0e1a44
alexander4545 <51681398+alexander4545@users.noreply.github.com>
2019-06-11 11:48:37 -0500

    Update solucion.js

--- php7/solucion.js
+++ php7/solucion.js
@@ -1,37 +1,20 @@
-<?php
-     function calcula_densidad(){
-	$Pi= 3.141592;
-        $radioT= 6378e3;
-        $radioMe= 2439e3;
-        $areaT= 4*$Pi*$radioT*$radioT;
-        $areaMe= 4*$Pi*$radioMe*$radioMe;
-        $mayorMe= $areaT/$areaMe;
-        return $mayorMe;
-     }
-function calcula_densidad2(){
-	$Pi= 3.141592;
-        $radioT= 6378e3;
-        $radioMa= 3390e3;
-        $areaT= 4*$Pi*$radioT*$radioT;
-        $areaMa= 4*$Pi*$radioMa*$radioMa;
-        $mayorMa= $areaT/$areaMa;
-        return $mayorMa;
-     }
-function calcula_densidad3(){
-	$Pi= 3.141592;
-        $radioT= 6378e3;
-        $radioVe= 6051e3;
-        $areaT= 4*$Pi*$radioT*$radioT;
-        $areaVe= 4*$Pi*$radioVe*$radioVe;
-        $mayorVe= $areaT/$areaVe;
-        return $mayorVe;
-     }
-?>
-<section class="resultado">
-<h2>Resultado:</h2>
-<div id="resultado"></div>
-<?php
- print "<h1>".calcula_densidad()." veces<br>"
- .calcula_densidad3()." veces<br>"
- .calcula_densidad2()." veces</h1>";
-?>
+<function calcula_densidad(){
+
+        var radioT= 6378e3;
+        var radioMe= 2439e3;
+        var radioMa= 3390e3;
+        var radioVe= 6051e3;
+        var areaT= 4*Math.PI*radioT*radioT;
+        var areaMe= 4*Math.PI*radioMe*radioMe;
+        var areaMa= 4*Math.PI*radioMa*radioMa;
+        var areaVe= 4*Math.PI*radioVe*radioVe;
+        var mayorMe= areaT/areaMe;
+        var mayorVe= areaT/areaVe;
+        var mayorMa= areaT/areaMa;
+        var d=document.getElementById("resultadoA");
+        d.innerHTML=mayorMe + ' veces';
+        var e=document.getElementById("resultadoB");
+        e.innerHTML=mayorVe + ' veces';
+        var f=document.getElementById("resultadoC");
+        f.innerHTML=mayorMa + ' veces';
+        }
